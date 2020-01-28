873fe68310098aa6edc1484a0a58abe4dc8acc7e
mathmagiya <mathmagiya@gmail.com>
2020-01-28 10:21:19 +0200

    Create solution.py

---
+++ expressions/sqrt(a+sqrt(a+sqrt(a)+.../solution.py
@@ -0,0 +1,16 @@
+import math
+
+times = int(input("Enter the number "))
+a1="math.sqrt(20+"
+
+plus="+"
+braket=")"
+string=""
+n=5
+for i in range(1,times+1):
+  string=string+a1
+
+
+string=string[:-1]+braket*times
+
+print(eval(string))
